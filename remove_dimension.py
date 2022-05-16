9a27fade599f57fd7af8bd280e194d260ca2cb2a
EitanAb8 <88735044+EitanAb8@users.noreply.github.com>
2022-05-16 05:02:16 +0200

    Finished exercise 12

---
+++ remove_dimension.py
@@ -0,0 +1,15 @@
+import numpy as np
+from numpy import array
+
+
+def remove_dimension(matrix: array) -> array:
+    """
+    The function removes a single-dimensional entries from a specified shape.
+    :param matrix: A given matrix.
+    :return: The matrix with less dimensional entry.
+    """
+    return np.squeeze(matrix).shape
+
+
+if __name__ == "__main__":
+    print(remove_dimension([[1, 2, 3], [4, 5, 6]]))
